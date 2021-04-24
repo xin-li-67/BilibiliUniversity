752479466c25c162171520ad6841abeac7acfee4
WilliamLeeBravo <williamlee.xin@gmail.com>
2021-04-25 07:20:30 +0800

    04252021

--- virtual_assistant/pyttsx3_example.py
+++ virtual_assistant/pyttsx3_example.py
@@ -7,7 +7,6 @@ rate = engine.getProperty('rate')   # getting details of current speaking rate
 print (rate)                        #printing current voice rate
 engine.setProperty('rate', 125)     # setting up new voice rate
 
-
 """VOLUME"""
 volume = engine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
 print (volume)                          #printing current volume level
--- virtual_assistant/wxpython.py
+++ virtual_assistant/wxpython.py
@@ -4,19 +4,21 @@ class MyFrame(wx.Frame):
     def __init__(self):
         wx.Frame.__init__(self, None,
             pos=wx.DefaultPosition, size=wx.Size(450,100),
-            style=wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION |
-             wx.CLOSE_BOX | wx.CLIP_CHILDREN,
+            style=wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX | wx.CLIP_CHILDREN,
             title="Serana")
         panel = wx.Panel(self)
         my_sizer = wx.BoxSizer(wx.VERTICAL)
         lbl = wx.StaticText(panel,
         label="Hello I am Serana. How can I help you?")
         my_sizer.Add(lbl, 0, wx.ALL, 5)
+
         self.txt = wx.TextCtrl(panel, style=wx.TE_PROCESS_ENTER,size=(400,30))
         self.txt.SetFocus()
         self.txt.Bind(wx.EVT_TEXT_ENTER, self.OnEnter)
+
         my_sizer.Add(self.txt, 0, wx.ALL, 5)
         panel.SetSizer(my_sizer)
+        
         self.Show()
 
     def OnEnter(self, event):
